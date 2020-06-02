c39e63ccde48e4176b31de9050b27193ea3a287b
Bhavana C <bhavana.chauhan@twinspark.co>
2020-06-02 20:21:52 +0530

    links

--- wp-content/themes/bc-chimney/page-templates/common/bc-copyright.php
+++ wp-content/themes/bc-chimney/page-templates/common/bc-copyright.php
@@ -8,7 +8,8 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <?php echo bc_get_theme_mod('bc_theme_options', 'footer','copyright_text', '<div class="container-fluid bc_footer_copyright_bar text-left py-md-4 py-lg-2 py-2 bc_font_alt_1 bc_line_height_24 bc_text_normal bc_text_16 ">
    <div class="container"><div class="row"><div class="col-md-12"><div class="copyright_border"><p class="text-md-left text-center color_1 bc_font_alt_1 bc_line_height_24 bc_text_normal bc_text_16 mb-md-1 mb-3 bc_sm_text_15 bc_sm_line_height_20 text-uppercase">VA hic: 2705172164 | Mhic: 93924 <span class="d-none d-md-inline-block">|</span> <span class="d-block d-md-inline-block">DCRA: 420218000219 | WSSC: 462</span></p><p class="text-md-left text-center color_2 bc_font_alt_1 bc_line_height_24 bc_text_normal bc_text_16 bc_sm_text_13 bc_sm_line_height_16">© '.date("Y").' Chimney.com <span class="d-inline-block">&nbsp;|&nbsp;</span> <span class="d-inline-block">All Rights Reserved</span>
-  <span class="d-none d-lg-inline-block d-none">&nbsp; | &nbsp;</span><span class="d-md-inline-block d-block"><a class="color_2 bc_font_alt_1 bc_line_height_24 bc_text_normal bc_text_16 no_hover_underline" href="#" data-toggle="modal" data-target="#disclaimer">Disclaimer</a><span class="d-inline-block">&nbsp; | &nbsp;</span><a class="color_2 bc_font_alt_1 bc_line_height_24 bc_text_normal bc_text_16 no_hover_underline" href="#">Privacy Policy</a></span></p></div></div></div></div>
+  
+  <span class="d-none d-lg-inline-block d-none">&nbsp; | &nbsp;</span><span class="d-md-inline-block d-block"><a class="color_2 bc_font_alt_1 bc_line_height_24 bc_text_normal bc_text_16 no_hover_underline" href="#" data-toggle="modal" data-target="#disclaimer">Disclaimer</a><span class="d-inline-block">&nbsp; | &nbsp;</span><a class="color_2 bc_font_alt_1 bc_line_height_24 bc_text_normal bc_text_16 no_hover_underline" href="'.get_home_url().'/privacy-policy/">Privacy Policy</a></span></p></div></div></div></div>
 </div>');?>
 
 
